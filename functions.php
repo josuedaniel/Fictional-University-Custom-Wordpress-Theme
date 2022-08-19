bb9d5ba2bc57ba0f0dd260b062ac0435799a2968
unknown <josuedaniel831@yahoo.com>
2022-08-19 06:47:10 -0700

    adding project to github

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+function university_files() {
+    wp_enqueue_script('main-university-javascript', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+    wp_enqueue_style('font-awesome', 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css' );
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css')); 
+}   
+
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features() {
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'university_features');
+?>
