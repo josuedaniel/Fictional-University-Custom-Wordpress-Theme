ada430587d0f8ff5e2e3efb49c2d7e1da39a78cc
unknown <josuedaniel831@yahoo.com>
2022-08-19 07:15:19 -0700

    added dynamic title to the metabox showing on the about subpages

--- page.php
+++ page.php
@@ -2,6 +2,7 @@
 get_header(); 
     while (have_posts()) {
         the_post(); ?>
+
         <div class="page-banner">
             <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>"></div>
             <div class="page-banner__content container container--narrow">
@@ -15,7 +16,8 @@ get_header();
             <div class="container container--narrow page-section">
                 <div class="metabox metabox--position-up metabox--with-home-link">
                     <p>
-                    <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+                    <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" 
+                    aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span>
                     </p>
                 </div>
                 <!--
