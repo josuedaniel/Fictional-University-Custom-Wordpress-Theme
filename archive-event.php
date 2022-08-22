0517e6912b6e9ac692472050538d678b7bff7729
unknown <josuedaniel831@yahoo.com>
2022-08-22 12:20:32 -0700

    added a past events archive paage and set up pagination

--- archive-event.php
+++ archive-event.php
@@ -31,13 +31,18 @@ get_header(); ?>
         <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php 
             the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">
+            <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" 
+                class="nu gray">
             Learn more</a></p>
         </div>
     </div>
 <?php }
 echo paginate_links();
 ?>
+<hr class="section-break">
+<!-- Add a link to the past events page -->
+<p>Looking for a recap of past events? <a href="<?php echo site_url('/?page_id=55'); ?>">Check out our 
+    past events archive.</a></p>
 </div>
 <?php get_footer();
 
---
+++ page-55.php
@@ -0,0 +1,79 @@
+<?php
+
+get_header(); ?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo
+            get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>    
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title">Past Events</h1>
+            <div class="page-banner__intro">
+                <p>A recap of our past events.</p>
+            </div>
+        </div>
+    </div>
+
+<div class="container container--narrow page-section">
+<?php 
+    // set variable names today to the current date
+    $today = date('Ymd');
+                    
+    // Create a custom query to show past events and assign it to the variable $pastEvents
+    $pastEvents = new WP_Query(array(
+        //Gets us the page number of the reults in the archive events page
+        'paged' => get_query_var('paged', 1),
+        // Grab the event post type and not post or page post type
+        'post_type' => 'event',
+        // tell wordpress the name of the custom field we want to use in the orderby key
+        'meta_key' => 'event_date',
+        // determines the order in which posts show 
+        'orderby' => 'meta_value_num',
+        // determines the order 
+        'order' => 'ASC',
+        //query the meta data of the custom post type Event
+        'meta_query' => array(
+            array(
+                //Meta key
+                'key' => 'event_date',
+                // Compres the value of the meta key less than 
+                'compare' => '<',
+                // Compares meta key value to the value of the variable $today
+                'value' => $today,
+                // sets the type of camparison to numeric
+                'type' => 'numeric'
+            )
+        )
+    ));
+    
+    // Wordpress loop, make sure its only looking in the variable $pastEvents
+    while($pastEvents->have_posts()) {
+        $pastEvents->the_post(); ?>
+        <div class="event-summary">
+            <a href="<?php the_permalink(); ?>" class="event-summary__date t-center">
+                <span class="event-summary__month"><?php 
+                    //show the custom chosen event_date through acf's get_field and format it to up as M
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M')
+                    
+                ?></span>
+                <span class="event-summary__day"><?php 
+                    //show the custom chosen event_date through acf's get_field and format it to up as d                                    
+                    echo $eventDate->format('d')
+                ?></span>
+            </a>
+            <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php 
+                the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">
+                Learn more</a></p>
+            </div>
+        </div>
+<?php }
+// Make sure the pagination pulls from the $pastEvents 
+echo paginate_links(array(
+    'total' => $pastEvents->max_num_pages
+));
+?>
+</div>
+<?php get_footer();
+
+?>
