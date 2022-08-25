792e489d9ceebd951917a5b1e8eceb3962c09252
unknown <josuedaniel831@yahoo.com>
2022-08-25 10:37:24 -0700

    Added page banner custom fields to single-professor

--- single-professor.php
+++ single-professor.php
@@ -3,12 +3,17 @@ get_header();
     while(have_posts()) {
         the_post(); ?>
         <div class="page-banner">
-            <div class="page-banner__bg-image" style="background-image: url(<?php echo 
-            get_theme_file_uri('/images/ocean.jpg'); ?>"></div>
+            <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = 
+                // use the custom field created in acf to bring in the image using the acf method get_field
+                get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner']; ?>)"></div>
             <div class="page-banner__content container container--narrow">
-                <h1 class="page-banner__title"><?php the_title(); ?> </h1>
+            <!-- You can use the following print_r to see the data behind something. in this case we use it to see that
+            $pageBannerImage is returned as an array and thats why we can use echo $pageBannerImage['url'] -->
+            <?php //print_r($pageBannerImage); ?>    
+            <h1 class="page-banner__title"><?php the_title(); ?> </h1>
                 <div class="page-banner__intro">
-                    <p>DON'T FORGET TO REPLACE ME LATER</p>
+                    <!-- pull in the meta field from one of the custom fields created in acf -->
+                    <p><?php the_field('page_banner_subtitle') ?></p>
                 </div>
             </div>
         </div>
